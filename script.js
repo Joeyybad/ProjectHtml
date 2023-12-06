e5fb7c2024ddc3d441468a58945dc304135d14f7
Joeyybad <jnkunga@hotmail.com>
2023-12-06 16:38:22 +0100

    page d'accueil+ajout image+organisation dossier

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let name = prompt("Entrez votre nom")
+/*let name = prompt("Entrez votre nom")
 
 if(name.length < 3 ){
     console.log(`votre nom ${name} est trop court`)
 }else{
     console.log(`bienvenue ${name}`)
-}
+}*/
